7c25ab9a4ded0ef78a36e610f47b7dd08e48a407
sameeranihathe <sameeranihathe@gmail.com>
2018-04-29 00:45:26 +0530

    login button click event added

--- Shopping-Cart/Admin/Login.aspx.cs
+++ Shopping-Cart/Admin/Login.aspx.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
@@ -10,5 +11,21 @@ public partial class Admin_Login : System.Web.UI.Page
     protected void Page_Load(object sender, EventArgs e)
     {
 
+    }
+
+    protected void btn_LoginClick(object sender, EventArgs e)
+    {
+        string LoginID = WebConfigurationManager.AppSettings["AdminLoginID"];
+        string Password = WebConfigurationManager.AppSettings["AdminPassword"];
+
+        if(txt_LoginID.Text == LoginID && txt_Password.Text == Password)
+        {
+            Session["ShoppingCartAdmin"] = "ShoppingCardAdmin";
+            Response.Redirect("~/Admin/AddNewProduct.aspx");
+        }
+        else
+        {
+            lbl_Alert.Text = "Incorrect Login ID or Password";
+        }
     }
 }
